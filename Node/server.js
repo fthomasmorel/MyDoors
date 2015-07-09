5e3853a14e23b6aea478db0b69def3fc8ea6fecf
fthomasmorel <florentthomasmorel@who-wanna.com>
2015-07-09 20:52:00 +0200

    Server.js

--- Node/server.js
+++ Node/server.js
@@ -1,6 +1,8 @@
 var io = require('socket.io')();
 //var rpio = new Rpio();
 
+console.log("Starting Server...");
+
 io.on('connection', function(socket){
   console.log("Connected");
   socket.on('portail-action', function(json) {
