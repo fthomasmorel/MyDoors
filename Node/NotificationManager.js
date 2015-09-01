e46e49c9ced7e819e7bd38e7cb84bf15555ccd90
fthomasmorel <fthomasm@insa-rennes.fr>
2015-09-01 14:23:29 +0200

    Add notification support

--- Node/NotificationManager.js
+++ Node/NotificationManager.js
@@ -8,13 +8,26 @@ var rl = readline.createInterface({
 
 var apn = require('apn');
 var options = {cert:"PushMyDoorsProdCert.pem",key:"PushMyDoorsProdKey.pem",passphrase:"Escargot35!",production:true};
-var conn  = new apn.Connection(options)
 
 var PortailManager = require('./PortailManager.js');
 var portailManager = new PortailManager();
 
-var state = !portailManager.isOpen();
+function prepareNotificationWithText(text){
+    rl.on('line',function(line){
+        sendNotification(line,text);
+    })
+}
 
+function sendNotification(token,text){
+  var conn  = new apn.Connection(options)
+  var dev   = new apn.Device(token)
+  var note  = new apn.Notification()
+  note.alert = text;
+  conn.pushNotification(note, dev)
+}
+
+
+var state = !portailManager.isOpen();
 if (state != portailManager.isOpen()){
     var text = "🔒Le portail s'est fermé";
     if (portailManager.isOpen()) { text = "🔓Le portail s'est ouvert" }
@@ -22,18 +35,3 @@ if (state != portailManager.isOpen()){
 }
 
 process.exit();
-
-
-function prepareNotificationWithText(text){
-    rl.on('line',function(line){
-        sendNotification(line,text);
-        sendNotification(line,text+"2");
-    })
-}
-
-function sendNotification(token,text){
-  var dev   = new apn.Device(token)
-  var note  = new apn.Notification()
-  note.alert = text;
-  conn.pushNotification(note, dev)
-}
